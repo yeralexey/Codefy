e47889e2538b4de23a4465506a3311a0b2c26f1a
loki <yer.alexey@gmail.com>
2023-02-03 20:08:41 +0300

    Works, buggy, non refactored.

--- plugins/registration.py
+++ plugins/registration.py
@@ -3,85 +3,145 @@ from main import *
 
 @Client.on_callback_query(filters.regex(pattern='proceed'))
 async def proceed_interview(client, call):
-
     user = await User.get_user(call.message.chat.id)
-    client.stop_listening((call.message.chat.id, call.message.from_user.id, call.message.id))
+
+    step_name = str(call.data).split("_")[1]
+
+    stepper_mark = False
+
+    get_step = Interview.get_step
+
 
     if "male" in call.data:
         await user.set_attribute("gender", str(call.data).split("_")[1])
-        step = await Interview.get_step("gender")
-        step = await Interview.get_step(step.next)
+        step = await get_step("gender")
+        step = await get_step(step.next)
     else:
-        step = await Interview.get_step(str(call.data).split("_")[1])
+        step = await get_step(step_name)
 
-    asking = True
-    while asking is True:
-        text = await step.get_main_text(call.message.chat.id)
-        backward_button = await step.get_previous(user.chosen_language)
-        forward_button = await step.get_next(user.chosen_language)
-        cancel_button = step.but_cancel
-        choice_button1 = step.but_main1
-        choice_button2 = step.but_main2
+    # creating text ---------------------------------------
 
-        keyboard, button_line1, button_line2 = None, None, None
+    text = await step.get_main_text(user.user_id)
+    backward_button = await step.get_previous(user.chosen_language)
+    forward_button = await step.get_next(user.chosen_language)
+    cancel_button = step.but_cancel
+    choice_button1 = step.but_main1
+    choice_button2 = step.but_main2
 
-        if forward_button and not backward_button:
-            button_line2 = [forward_button, cancel_button]
-        elif backward_button and not forward_button:
-            button_line2 = [backward_button, cancel_button]
-        elif forward_button and backward_button:
-            button_line2 = [backward_button, forward_button, cancel_button]
+    # creating keyboard -------------------------------------
 
-        if choice_button1 and not choice_button2:
-            button_line1 = [choice_button1]
-        elif choice_button1 and choice_button2:
-            button_line1 = [choice_button1, choice_button2]
+    keyboard, button_line1, button_line2 = None, None, None
 
-        if button_line1:
-            questiontype = "button"
-            keyboard = ikb([button_line1, button_line2])
+    if forward_button and not backward_button:
+        button_line2 = [forward_button, cancel_button]
+    elif backward_button and not forward_button:
+        button_line2 = [backward_button, cancel_button]
+    elif forward_button and backward_button:
+        button_line2 = [backward_button, forward_button, cancel_button]
+
+    if choice_button1 and not choice_button2:
+        button_line1 = [choice_button1]
+    elif choice_button1 and choice_button2:
+        button_line1 = [choice_button1, choice_button2]
+
+    if button_line1:
+        keyboard = ikb([button_line1, button_line2])
+        await user.set_attribute("current_step", None)
+    else:
+        keyboard = ikb([button_line2])
+        stepper_mark = True  # if there is only backward-forward buttons - so we await text input
+
+    if step.kill_all_buttons is True:
+        keyboard = None
+
+    # edit mesage    -------------------------------------------
+
+    msg = await Client.edit_message_text(client, call.message.chat.id, call.message.id,
+                                         text=text, reply_markup=keyboard, disable_web_page_preview=True)
+    if stepper_mark is True:
+        await user.set_attribute("current_step", (step.name, msg.chat.id, msg.id))
+
+
+@Client.on_message()
+async def proceed_interviev_text(client, message):
+    user = await User.get_user(message.chat.id)
+
+    read_step = await user.get_attribute("current_step")
+    if not read_step:
+        message.continue_propagation()
+        return
+
+    step_name = read_step[0]
+    msg = (read_step[1], read_step[2])
+
+    get_step = Interview.get_step
+
+    step = await get_step(step_name)
+
+    if message.text[0] == "/":
+        if message.text == "/start" or "/cancel":
+            text = plate("cancel", user.chosen_language)
+            await Client.edit_message_text(client, chat_id=msg[0], message_id=msg[1], text=text,
+                                           disable_web_page_preview=True)
+            await user.set_attribute("current_step", None)
+            return
         else:
-            questiontype = "input"
-            keyboard = ikb([button_line2])
+            msg = await message.reply(plate("service_hint2"), user.chosen_language)
+            await asyncio.sleep(1)
+            await Client.delete_messages(client, message.chat.id, [message.id, msg.id])
 
-        new = True
-        checking = True
-        while checking is True and asking is True:
-            if new:
-                await Client.edit_message_text(client, call.message.chat.id, call.message.id, text,
-                                               reply_markup=keyboard,
-                                               disable_web_page_preview=True)
-            if questiontype == "button":
-                return
+    else:
+        check = step.datatest if step.datatest is True else step.datatest(message.text)
 
-            try:
-                answer = await call.message.chat.listen(timeout=30)
-            except listen.ListenerStopped:
-                return
-            except listen.ListenerTimeout:
-                return
-            except Exception as err:
-                logger.exception(err)
-                return
+        if check is not True:
+            is_wrong_reply = await Client.send_message(client, message.chat.id, check)
+            await asyncio.sleep(3)
+            await Client.delete_messages(client, message.chat.id, [message.id, is_wrong_reply.id])
 
-            check = step.datatest if step.datatest is True else step.datatest(answer.text, user.chosen_language)
+        else:
+            await step.apenddata(user, str(step.name)[1:], message.text.replace(";", "").replace("+", ""))
 
-            if check is not True:
-                client.stop_listening((call.message.chat.id, call.message.from_user.id, call.message.id))
-                is_wrong_reply = await Client.send_message(client, call.message.chat.id, check)
-                await asyncio.sleep(3)
-                await Client.delete_messages(client, call.message.chat.id, [answer.id, is_wrong_reply.id])
-                new = False
+            step = await get_step(step.next)
 
-            elif answer.text[0] == "/":
-                msg = await answer.reply(plate("service_hint2", user.chosen_language))
-                await asyncio.sleep(1)
-                await Client.delete_messages(client, call.message.chat.id, [answer.id, msg.id])
-                new = False
+            stepper_mark = False
 
+            # creating text ---------------------------------------
+
+            text = await step.get_main_text(user.user_id)
+            backward_button = await step.get_previous(user.chosen_language)
+            forward_button = await step.get_next(user.chosen_language)
+            choice_button1 = step.but_main1
+            choice_button2 = step.but_main2
+
+            # creating keyboard -------------------------------------
+
+            keyboard, button_line1, button_line2 = None, None, None
+
+            if forward_button and not backward_button:
+                button_line2 = [forward_button]
+            elif backward_button and not forward_button:
+                button_line2 = [backward_button]
+            elif forward_button and backward_button:
+                button_line2 = [backward_button, forward_button]
+
+            if choice_button1 and not choice_button2:
+                button_line1 = [choice_button1]
+            elif choice_button1 and choice_button2:
+                button_line1 = [choice_button1, choice_button2]
+
+            if button_line1:
+                keyboard = ikb([button_line1, button_line2])
+                await user.set_attribute("current_step", None)
             else:
-                await step.apenddata(user, step.name, answer.text.replace(";", "").replace("+", ""))
-                step = await Interview.get_step(step.next)
-                await Client.delete_messages(client, call.message.chat.id, [answer.id])
-                checking = False
-                
+                keyboard = ikb([button_line2])
+                stepper_mark = True  # if there is only backward-forward buttons - so we await text input
+
+            if step.kill_all_buttons is True:
+                keyboard = None
+
+            # edit mesage    -------------------------------------------
+
+            msg = await Client.edit_message_text(client, chat_id=msg[0], message_id=msg[1],
+                                                 text=text, reply_markup=keyboard, disable_web_page_preview=True)
+            if stepper_mark is True:
+                await user.set_attribute("current_step", (step.name, msg.chat.id, msg.id))
