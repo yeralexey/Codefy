37259ecb20d6673a09246c665f0c622c231fb9bb
loki <yer.alexey@gmail.com>
2023-02-08 14:39:05 +0300

    Alpha release.

--- plugins/inline.py
+++ plugins/inline.py
@@ -2,6 +2,30 @@ from main import *
 from utils.nekobin import *
 from uuid import uuid4
 nekobin = NekoBin()
+neko_uid = str(uuid4())
+python_uid = str(uuid4())
+from pyrogram import ContinuePropagation
+
+from aimodels import askopenai
+
+
+
+async def nekobin_it(content, title="test function", author="CodeAiBot"):
+    try:
+        neko = await nekobin.nekofy(
+            content=content,
+            title=title,
+            author=author
+        )
+    except HostDownError:
+        return "Host is down at the moment"
+    except TooFastError:
+        return "Too many requests"
+
+    if neko.url[-4:] != "None":
+        return neko.url + ".py"
+    else:
+        return "Nekobin.com refuse, flood wait."
 
 
 @Client.on_inline_query()
@@ -14,14 +38,29 @@ async def inline_answer(client, inline_query):
         results=[
             InlineQueryResultArticle(
                 title="Nekobin.com",
-                description="Elegant and open-source pastebin service, Paste, save and share the link of your text "
-                            "content using a sleek and intuitive interface!",
+                description="Paste, save and share the link of your python code  in elegant way. (len(code)<500)",
                 input_message_content=InputTextMessageContent("Generating...⏳"),
-                id=str(uuid4()),
+                id=neko_uid,
                 thumb_url="https://nekobin.com/static/img/nekobin.jpg",
                 thumb_width=5,
                 thumb_height=5,
-                url="Nekobin.com",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton(
+                            "Powered by Nekobin.com",
+                            url="https://Nekobin.com"
+                        )]
+                    ]
+                )
+            ),
+            InlineQueryResultArticle(
+                title="Python function",
+                description='Create python function, complete phrase "Function, that..."',
+                input_message_content=InputTextMessageContent("Generating...⏳"),
+                id=python_uid,
+                thumb_url="https://i.pinimg.com/564x/4d/13/d5/4d13d55d6d0b38b7a4e85fcf97ff6279.jpg",
+                thumb_width=5,
+                thumb_height=5,
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [InlineKeyboardButton(
@@ -37,7 +76,11 @@ async def inline_answer(client, inline_query):
 
 
 @Client.on_chosen_inline_result()
-async def in_line_result(client, inline_result):
+async def in_line_result_neko(client, inline_result):
+    if inline_result.result_id != neko_uid:
+        raise ContinuePropagation
+        return
+
     user = await User.get_user(user_id=inline_result.from_user.id)
     code_to_paste = inline_result.query
     result = await nekobin_it(content=code_to_paste, author=user.user_name)
@@ -57,19 +100,34 @@ async def in_line_result(client, inline_result):
     #                                       reply_markup=None)
 
 
-async def nekobin_it(content, title="test function", author="CodeAiBot"):
-    try:
-        neko = await nekobin.nekofy(
-            content=content,
-            title=title,
-            author=author
-        )
-    except HostDownError:
-        return "Host is down at the moment"
-    except TooFastError:
-        return "Too many requests"
 
-    if neko.url[-4:] != "None":
-        return neko.url + ".py"
+
+@Client.on_chosen_inline_result()
+async def in_line_result(client, inline_result):
+    if inline_result.result_id != python_uid:
+        raise ContinuePropagation
+        return
+
+    user = await User.get_user(user_id=inline_result.from_user.id)
+
+    if len(str(inline_result.query)) < 25:
+        text = "provide sufficient description, more 25 chars"
     else:
-        return "Nekobin.com refuse, flood wait."
+        question = f"###Write a python function with an elaborate, high quality docstrings, explaining what the " \
+                   f"function does and how to use it, that {inline_result.query}:"
+
+        answer = await askopenai.ask_code(question=question, identificator=f"test~{user.user_id}", prompt_final=None)
+        answer = answer["choices"][0]["text"]
+        answer = f"```{str(answer).replace('`', '')}```"
+
+        result = await nekobin_it(content=answer, author=user.user_name)
+        text = f"Function,that **{inline_result.query}:**\n{answer}\n\n{result}"
+
+    await Client.edit_inline_text(client, inline_message_id=inline_result.inline_message_id, text=text,
+                                  disable_web_page_preview=True)
+    # await Client.edit_inline_reply_markup(client, inline_message_id=inline_result.inline_message_id,
+    #                                       reply_markup=None)
+
+
+
+
