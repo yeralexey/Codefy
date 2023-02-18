5522f75c540240c959558c0919571ea5e64d4c20
loki <yer.alexey@gmail.com>
2023-02-18 11:58:51 +0300

    Keyboard helper plugin.

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,6 +3,8 @@ from .validation import InputValidator
 from .database import DatabaseWriter
 from .system import SystemController
 from .googlejobs import GoogleWorker
+from .keyboard import ikb
+
 
 from .logger import init_logger
 logger = init_logger('main')
@@ -13,6 +15,7 @@ plate = Plate()
 vd = InputValidator()
 db = DatabaseWriter()
 cr = SystemController(config.name)
+keyboard = ikb
 
 try:
     gw = GoogleWorker(config.google_credentials, config.main_sheet)
